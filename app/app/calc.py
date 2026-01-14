98092035072eda5fa1073180fbab73a13d4aa7d3
Bmo411 <oceguedaalejandro12@gmail.com>
2026-01-14 13:43:14 -0600

    configured docker compose and checks to wait fro db

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,11 @@ Calculator functions
 """
 
 
-def add(x,y):
+def add(x, y):
     """Add function"""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract function"""
     return x - y
